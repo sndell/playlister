b8723afddcc0195f3897523ff84a6ba2c1cddcbc
sndell <dev.besu@gmail.com>
2024-12-21 23:34:17 +0100

    feat: make redirect https if in prod

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -5,11 +5,10 @@ export async function GET() {
   const oauth2Client = new google.auth.OAuth2(
     process.env.GOOGLE_CLIENT_ID,
     process.env.GOOGLE_CLIENT_SECRET,
-    `${process.env.APP_URL}/api/auth/callback`
+    `${process.env.NODE_ENV === 'production' ? 'https://' : ''}${process.env.APP_URL}/api/auth/callback`
   );
 
   const scopes = ['https://www.googleapis.com/auth/youtube.readonly'];
-
 
   const url = oauth2Client.generateAuthUrl({
     access_type: 'offline',
